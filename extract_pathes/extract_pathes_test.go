850a5af3aec7c5df244645ee7978dc49ddb8467b
dmitrii.kutin <dmitrii.kutin@tages.ru>
2024-11-05 13:05:30 +0300

    feat(extract_pathes): added couple of new tests, updated logic of expected and real results comparison

--- extract_pathes/extract_pathes_test.go
+++ extract_pathes/extract_pathes_test.go
@@ -3,31 +3,67 @@ package extract_pathes
 import (
 	S "do-swagger/structs"
 	"reflect"
+	"sort"
 	"testing"
 )
 
 func TestExtractPathes(t *testing.T) {
-	postOnlyMethod := map[string]map[string]interface{}{
-		"post": {
-			"parameters": []interface{}{},
-			"responses": map[string]map[string]string{
-				"200": {
-					"description": "OK",
+	oneEndpointMap := map[string]map[string]interface{}{
+		"path/1": {
+			"post": map[string]interface{}{
+				"parameters": []interface{}{},
+				"responses": map[string]map[string]string{
+					"200": {
+						"description": "OK",
+					},
 				},
 			},
 		},
 	}
 
-	oneEndpointMap := map[string]interface{}{
-		"path/1": postOnlyMethod,
+	coupleEndpointMap := map[string]map[string]interface{}{
+		"path/1": {
+			"get": map[string]interface{}{
+				"parameters": []interface{}{},
+				"responses": map[string]map[string]string{
+					"200": {
+						"description": "OK",
+					},
+				},
+			},
+			"post": map[string]interface{}{
+				"parameters": []interface{}{},
+				"responses": map[string]map[string]string{
+					"200": {
+						"description": "OK",
+					},
+				},
+			},
+		},
+		"path/2": {
+			"post": map[string]interface{}{
+				"parameters": []interface{}{},
+				"responses": map[string]map[string]string{
+					"200": {
+						"description": "OK",
+					},
+				},
+			},
+		},
 	}
 
 	cases := []struct {
-		in   map[string]interface{}
+		in   map[string]map[string]map[string]interface{}
 		want []S.PathMethod
 	}{
 		{
-			map[string]interface{}{
+			map[string]map[string]map[string]interface{}{
+				"paths": {},
+			},
+			[]S.PathMethod{},
+		},
+		{
+			map[string]map[string]map[string]interface{}{
 				"paths": oneEndpointMap,
 			},
 			[]S.PathMethod{
@@ -37,12 +73,44 @@ func TestExtractPathes(t *testing.T) {
 				},
 			},
 		},
+		{
+			map[string]map[string]map[string]interface{}{
+				"paths": coupleEndpointMap,
+			},
+			[]S.PathMethod{
+				{
+					Path:   "path/1",
+					Method: "get",
+				},
+				{
+					Path:   "path/1",
+					Method: "post",
+				},
+				{
+					Path:   "path/2",
+					Method: "post",
+				},
+			},
+		},
 	}
 
 	for _, currentCase := range cases {
 		got, _ := ExtractPathes(currentCase.in)
 
-		if !reflect.DeepEqual(got, currentCase.want) {
+		wantLen := len(currentCase.want)
+		gotLen := len(got)
+
+		if gotLen != wantLen {
+			t.Errorf("Expected array length isn't equal result\ngot %d\nwant %d", gotLen, wantLen)
+		}
+
+		comparePathMethods := func(i, j int) bool {
+			return got[i].Path < got[j].Path && got[i].Method < got[j].Method
+		}
+		sort.Slice(got[:], comparePathMethods)
+		sort.Slice(currentCase.want[:], comparePathMethods)
+
+		if gotLen != 0 && !reflect.DeepEqual(got, currentCase.want) {
 			t.Errorf("Testing ExtractPathes(%+v)\ngot %+v\nwant %+v", currentCase.in, got, currentCase.want)
 		} else {
 			t.Log("Passed")
