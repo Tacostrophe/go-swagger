f44afc4539d1c5845f3c45a3699ba8264fc5081f
dmitrii.kutin <dmitrii.kutin@tages.ru>
2024-11-05 13:07:13 +0300

    feat(extract_pathes): added logic of extracting pathes

--- extract_pathes/extract_pathes.go
+++ extract_pathes/extract_pathes.go
@@ -3,17 +3,23 @@ package extract_pathes
 import (
 	S "do-swagger/structs"
 	"errors"
-	"fmt"
 )
 
-func ExtractPathes(swagger map[string]interface{}) ([]S.PathMethod, error) {
+func ExtractPathes(swagger map[string]map[string]map[string]interface{}) ([]S.PathMethod, error) {
 	pathes, ok := swagger["paths"]
 	if !ok {
 		return []S.PathMethod{}, errors.New("swagger must have pathes")
 	}
 
-	fmt.Printf("pathes: %v\n", pathes)
-	fmt.Printf("pathes len: %d\n", len(pathes.(map[string]interface{})))
-
-	return []S.PathMethod{}, nil
+	var pathesMethods []S.PathMethod
+	for path, methods := range pathes {
+		for method := range methods {
+			currentPathMethod := S.PathMethod{
+				Path:   path,
+				Method: method,
+			}
+			pathesMethods = append(pathesMethods, currentPathMethod)
+		}
+	}
+	return pathesMethods, nil
 }
