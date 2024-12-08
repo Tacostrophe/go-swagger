fdc7eb54388f525e78a0bb3f68c2a35d81fcce19
dmitrii.kutin <dmitrii.kutin@tages.ru>
2024-12-08 20:51:28 +0300

    feat(structs): added FirstTag field to PathMethod struct

--- structs/swagger.go
+++ structs/swagger.go
@@ -1,18 +1,10 @@
 package structs
 
 type PathMethod struct {
-	Path   string
-	Method string
+	Path     string
+	Method   string
+	FirstTag string
 }
-
-// type SwaggerInfo struct {
-// 	title          string
-// 	description    string
-// 	termsOfService string
-// contact        Contact
-// license        License
-// 	version string
-// }
 
 type Swagger struct {
 	Paths map[string]map[string]interface{} `json: "paths"`
