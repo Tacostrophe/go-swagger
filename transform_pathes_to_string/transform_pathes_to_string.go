4c559a1cbc0cfc6152e5f52e053cf274dec591a0
dmitrii.kutin <dmitrii.kutin@tages.ru>
2024-12-08 20:53:12 +0300

    feat(transform_pathes_to_string): added devision by tag into logic of transformation to string

--- transform_pathes_to_string/transform_pathes_to_string.go
+++ transform_pathes_to_string/transform_pathes_to_string.go
@@ -8,46 +8,106 @@ import (
 	S "github.com/Tacostrophe/go-swagger/structs"
 )
 
+func groupPathesByFirstTag(pathesMethodes []S.PathMethod) [][]S.PathMethod {
+	// expected income array to be sorted by first tag
+	groupedPathesMethodes := [][]S.PathMethod{}
+	if len(pathesMethodes) == 0 {
+		return groupedPathesMethodes
+	}
+
+	currentFirstTag := pathesMethodes[0].FirstTag
+	currentFirstTagStartIdx := 0
+
+	for idx := 1; idx < len(pathesMethodes); idx++ {
+		if pathMethod := pathesMethodes[idx]; currentFirstTag != pathMethod.FirstTag {
+			tagName := currentFirstTag
+			if tagName == "" {
+				tagName = "*Tagless*"
+			}
+			groupedPathesMethodes = append(groupedPathesMethodes, pathesMethodes[currentFirstTagStartIdx:idx])
+			currentFirstTag = pathMethod.FirstTag
+			currentFirstTagStartIdx = idx
+		}
+	}
+	groupedPathesMethodes = append(groupedPathesMethodes, pathesMethodes[currentFirstTagStartIdx:])
+	return groupedPathesMethodes
+}
+
 func TransformPathesToString(pathesMethodes []S.PathMethod) (string, error) {
 	pathesAmount := len(pathesMethodes)
 	if pathesAmount == 0 {
 		return "", errors.New("swagger must have at least one path")
 	}
-	rowsAmount := 50
-	maxRowLengths := make([]int, pathesAmount/rowsAmount)
+	columnsAmount := 3
 	minIndentation := 5
-	if pathesAmount < rowsAmount {
-		rowsAmount = pathesAmount
+
+	groupedPathes := groupPathesByFirstTag(pathesMethodes)
+	rowsAmount := 0
+	staticCharsAmountInPathString := 6
+	maxRowLengths := make([]int, columnsAmount-1)
+	for _, tagsPathes := range groupedPathes {
+		currentTagMaxRowsPerColumn := (len(tagsPathes) / columnsAmount)
+		if len(tagsPathes)%columnsAmount > 0 {
+			currentTagMaxRowsPerColumn += 1
+		}
+		rowsAmount += 1 + currentTagMaxRowsPerColumn
+		for tagPathIdx, tagPath := range tagsPathes {
+			columnIdx := tagPathIdx / currentTagMaxRowsPerColumn
+			currentRowLen := len(tagPath.Method) + len(tagPath.Path) + staticCharsAmountInPathString + minIndentation
+			if columnIdx < len(maxRowLengths) && currentRowLen > maxRowLengths[columnIdx] {
+				maxRowLengths[columnIdx] = currentRowLen
+			}
+		}
+	}
+	for mrlIdx := range maxRowLengths {
+		if mrlIdx == 0 {
+			continue
+		}
+
+		maxRowLengths[mrlIdx] += maxRowLengths[mrlIdx-1]
 	}
 
 	pathesRows := make([]string, rowsAmount)
 
-	for idx, pathMethod := range pathesMethodes {
-		currentPath := pathMethod.Path
-
-		// don't write path if previous is the same
-		if isFirstElement := idx == 0; !isFirstElement && currentPath == pathesMethodes[idx-1].Path {
-			currentPath = strings.Repeat(" ", len(currentPath))
+	pathIdx := 0
+	rowIdx := 0
+	for _, tagPathes := range groupedPathes {
+		tag := tagPathes[0].FirstTag
+		if tag == "" {
+			tag = "*Tagless*"
+		}
+		pathesRows[rowIdx] = tag
+		rowIdx += 1
+		rowsPerColumn := (len(tagPathes) / columnsAmount)
+		if len(tagPathes)%columnsAmount > 0 {
+			rowsPerColumn += 1
 		}
 
-		currentPathStr := fmt.Sprintf(
-			"%3d. %s %s",
-			idx,
-			currentPath,
-			strings.ToUpper(pathMethod.Method),
-		)
+		for tagPathIdx, pathMethod := range tagPathes {
+			currentPath := pathMethod.Path
 
-		rowIdx := idx % rowsAmount
-		columnIdx := idx / rowsAmount
-		if columnIdx > 0 && columnIdx <= len(maxRowLengths) {
-			indentation := strings.Repeat(" ", maxRowLengths[columnIdx-1]-len(pathesRows[rowIdx]))
-			currentPathStr = indentation + currentPathStr
+			// don't write path if previous is the same
+			if isFirstElement := tagPathIdx%rowsPerColumn == 0; !isFirstElement && currentPath == tagPathes[tagPathIdx-1].Path {
+				currentPath = strings.Repeat(" ", len(currentPath))
+			}
+
+			currentPathStr := fmt.Sprintf(
+				"%3d. %s %s",
+				pathIdx,
+				currentPath,
+				strings.ToUpper(pathMethod.Method),
+			)
+			pathIdx += 1
+
+			currentRowIdx := (tagPathIdx % rowsPerColumn) + rowIdx
+			columnIdx := tagPathIdx / rowsPerColumn
+			if columnIdx > 0 && columnIdx <= len(maxRowLengths) {
+				indentation := strings.Repeat(" ", maxRowLengths[columnIdx-1]-len(pathesRows[currentRowIdx]))
+				currentPathStr = indentation + currentPathStr
+			}
+			pathesRows[currentRowIdx] += currentPathStr
 		}
-		pathesRows[rowIdx] += currentPathStr
-		currentRowLen := len(pathesRows[rowIdx]) + minIndentation
-		if columnIdx < len(maxRowLengths) && currentRowLen > maxRowLengths[columnIdx] {
-			maxRowLengths[columnIdx] = currentRowLen
-		}
+		rowIdx += rowsPerColumn
 	}
 
 	return strings.Join(pathesRows, "\n"), nil
